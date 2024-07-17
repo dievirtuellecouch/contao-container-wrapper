8166630dfe8620aaad3d30e6b115844f8f549d8d
Jonathan Hermann <github@dievirtuellecouch.net>
2024-07-17 12:52:56 +0200

    Add legend translations

---
+++ src/Resources/contao/languages/en/tl_content.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_content']['container_wrapper_container_legend'] = 'Container selection';
+$GLOBALS['TL_LANG']['tl_content']['container_wrapper_variant_legend'] = 'Container variants ';
